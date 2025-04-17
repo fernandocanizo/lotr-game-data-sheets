e7bb1d5f8a121d041438665bb7e2f3c69435843a
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2025-04-16 22:05:58 -0300

    Change link to point to home page
    But it's not working. Reloaded it, tried it in an incognito window, and
    it still remains on the same page but adding a hash (#).

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div class="h-96 w-full overflow-scroll">
       <nav class="rounded-lg border shadow-lg overflow-hidden p-2 bg-white border-stone-200 shadow-stone-950/5 sticky top-0 mx-auto w-full max-w-screen-xl">
         <div class="flex items-center">
-          <a href="#" class="font-sans antialiased text-sm text-current ml-2 mr-2 block py-1 font-semibold">LOTR Data Sheets</a>
+          <a href="/" class="font-sans antialiased text-sm text-current ml-2 mr-2 block py-1 font-semibold">LOTR Data Sheets</a>
           <hr class="ml-1 mr-1.5 hidden h-5 w-px border-l border-t-0 border-secondary-dark lg:block" />
           <div class="hidden lg:block">
             <ul class="mt-4 flex flex-col gap-x-3 gap-y-1.5 lg:mt-0 lg:flex-row lg:items-center">
